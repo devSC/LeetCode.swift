4ff07431d48b7d4dd8a51d8b6f83c15e4c6e95c9
Wilson Yuan <yuansc@nihao.com>
2017-03-02 14:22:38 +0800

    add [Add Two Numbers] solution

---
+++ 2. Add Two Numbers.playground/Contents.swift
@@ -0,0 +1,88 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+
+ */
+
+public class ListNode: CustomStringConvertible {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+    
+    public var description: String {
+        return "val: \(val), next: \(next)"
+    }
+}
+
+let node1 = ListNode(2)
+let node2 = ListNode(4)
+let node3 = ListNode(3)
+node2.next = node3
+node1.next = node2
+
+let node4 = ListNode(5)
+let node5 = ListNode(6)
+let node6 = ListNode(1)
+node5.next = node6
+node4.next = node5
+
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+        guard let l1 = l1, let l2 = l2 else {
+            return nil
+        }
+        
+        func nodeNumbers(_ node: ListNode?) -> [Int] {
+            var node1Numbers = [Int]()
+            var node = node
+            while node != nil {
+                node1Numbers.append(node!.val)
+                node = node!.next
+            }
+            return node1Numbers
+        }
+        
+        let node1Number = nodeNumbers(l1)
+        let node2Number = nodeNumbers(l2)
+        
+        var result: ListNode?
+        var lastNode: ListNode?
+        var upValue: Int = 0
+        
+        for (index, item) in node1Number.enumerated() {
+            //
+            let node2Item = node2Number[index]
+            var value = item + node2Item + upValue
+            print(value)
+            if value == 10 {
+                value = 0
+                upValue = 1;
+            }
+            else {
+                upValue = 0;
+            }
+            let node = ListNode(value)
+            if (lastNode != nil) {
+                lastNode!.next = node
+                lastNode = node
+            }
+            else {
+                result = node
+                lastNode = node
+            }
+            
+        }
+        return result
+    }
+}
+
+let solution = Solution()
+solution.addTwoNumbers(node1, node4)
