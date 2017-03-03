6d4929cf9963617a3bd435d34442ef8ebff7a679
Wilson Yuan <yuansc@nihao.com>
2017-03-03 18:10:48 +0800

    Init [Longest substring without repeating characters]

---
+++ 3. Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    return 0
+}
