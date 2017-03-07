2c537791fe1ff1ba84bfb1d111b63bdffa7b6958
Wilson Yuan <yuansc@nihao.com>
2017-03-07 10:38:48 +0800

    update [Median of Two Sorted Arrays]

--- 4. Median of Two Sorted Arrays.playground/Contents.swift
+++ 4. Median of Two Sorted Arrays.playground/Contents.swift
@@ -18,3 +18,53 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 */
+
+
+func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+    
+    var nums: [Int] = []
+    nums.append(contentsOf: nums1)
+    nums.append(contentsOf: nums2)
+    
+    var values = nums.sorted { (first, second) -> Bool in
+        return first < second
+    }
+    
+    if values.count == 0 {
+        return 0
+    }
+    else if values.count % 2 == 0 {
+        //even number
+        let cIdx = Double(values.count / 2)
+        //is last object eg: [0, 1]
+        if Int(cIdx) == values.count - 1 {
+            return Double(values.reduce(0) { $0 + $1 }) / 2.0
+        }
+        else {
+            let median = values[Int(cIdx)] + values[Int(cIdx - 1)]
+            return Double(median) / 2.0
+        }
+    }
+    else {
+        //odd number
+        let cIdx = Double(values.count / 2)
+        return Double(values[Int(cIdx)])
+    }
+}
+
+var nums1: [Int] = [1, 2]
+var nums2: [Int] = [3, 4]
+findMedianSortedArrays(nums1, nums2)
+
+nums1 = [1, 3]
+nums2 = [2]
+findMedianSortedArrays(nums1, nums2)
+
+nums1 = []
+nums2 = [3]
+findMedianSortedArrays(nums1, nums2)
+
+nums1 = [1, 1]
+nums2 = [1, 3]
+findMedianSortedArrays(nums1, nums2)
+
