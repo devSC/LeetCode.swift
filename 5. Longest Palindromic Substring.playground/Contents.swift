ec9f31ed6231548dd4c4ef46ec4dc1116d15631c
Wilson <xiaochong2154@163.com>
2017-03-08 23:39:55 +0800

    update [Longest Palindromic Substring]

--- 5. Longest Palindromic Substring.playground/Contents.swift
+++ 5. Longest Palindromic Substring.playground/Contents.swift
@@ -18,3 +18,41 @@ Input: "cbbd"
 Output: "bb"
  
 */
+
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        
+        guard s.characters.count >= 2 else {
+            return s
+        }
+        
+        var maxLength: Int = 0
+        var location: Int = 0
+        let characters = s.characters
+        
+        for (index, char) in characters.enumerated() {
+            
+            print("char: \(char), index: \(index)")
+            let charStartIndex = characters.index(of: char)!
+            
+            if index + 1 < characters.count {
+                let charEndIndex = characters.index(charStartIndex, offsetBy: 1)
+                let nextChar = characters[charEndIndex]
+                //
+                print("nextChar: \(nextChar)")
+            }
+
+//            let nextCharIndex = index + 1
+//            if nextCharIndex < characters.count {
+//                let nextChar = characters[nextCharIndex]
+//                
+//            }
+        }
+        
+        return s
+    }
+}
+
+let solution = Solution()
+
+solution.longestPalindrome("babad")
