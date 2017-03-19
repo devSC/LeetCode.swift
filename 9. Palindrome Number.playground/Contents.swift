7891b18ac1ff30f90294aa33b3009fd1bd5fe259
Wilson <xiaochong2154@163.com>
2017-03-19 21:16:42 +0800

    initial [Palindrome Number]

---
+++ 9. Palindrome Number.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: 9. Palindrome Number
+/*:
+ Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
+import Foundation
+
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        return true
+    }
+}
+
