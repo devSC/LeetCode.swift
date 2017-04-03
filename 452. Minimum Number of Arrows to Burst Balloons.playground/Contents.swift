c64ab73af3d4aa40cc305d9215215007aea54235
Wilson <xiaochong2154@163.com>
2017-04-03 19:29:49 +0800

    update [Minimum Number of Arrows to Burst Balloons]

--- 452. Minimum Number of Arrows to Burst Balloons.playground/Contents.swift
+++ 452. Minimum Number of Arrows to Burst Balloons.playground/Contents.swift
@@ -14,6 +14,41 @@
  
  Explanation:
  One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+ 
+ 这块总共有3种状态:
+ 1: A contain B
+ 2: A intersection B 
+ 3: A B ever seperate
+ 或者, 可以抽象成如下的数学问题：给定 n 个区间，求其一共有多少个相交的部分。
  */
 
 import Foundation
+
+class Solution {
+    
+    func findMinArrowShots(_ points: [[Int]]) -> Int {
+        
+        //sort
+        if points.count == 1 {
+            return 1
+        }
+        //sort with ascending
+        let sortedArray = points.sorted { (array1, array2) -> Bool in
+            return array1.last! < array2.last!
+        }
+        
+        var count = 1;
+        var min = sortedArray.first!.last!
+        
+        for point in sortedArray {
+            if point.first! >  min {
+                min = point.last!
+                count += 1
+            }
+        }
+        
+        return count;
+    }
+}
+
+let soulution = Solution().findMinArrowShots([[10,16], [2,8], [1,6], [7,12]])
