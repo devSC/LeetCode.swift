1e6631366dc49c68a450ae500a793be7c3c2780a
Wilson Yuan <yuansc@nihao.com>
2017-04-12 17:11:03 +0800

    update [Longest Word in Dictionary through Deleting]

--- 524. Longest Word in Dictionary through Deleting.playground/Contents.swift
+++ 524. Longest Word in Dictionary through Deleting.playground/Contents.swift
@@ -21,3 +21,47 @@
 */
 
 import Foundation
+
+class Solution {
+    func findLongestWord(_ s: String, _ d: [String]) -> String {
+        
+        //1. Sort
+        let sortedArray = d.sorted { (str1, str2) -> Bool in
+            //1.1 find the equal length strings and sort by charact value
+            if str1.characters.count == str2.characters.count {
+                return str1 < str2
+            }
+            else {
+                //1.2 sort the dic by the cout of characters
+                return str1.characters.count > str2.characters.count
+            }
+        }
+        sortedArray
+        
+        //2. find the longest word
+        let sCharacters: [Character] = Array(s.characters)
+        
+        for string in sortedArray {
+            
+            let stringCharactes: [Character] = Array(string.characters)
+            
+            //match
+            var index: Int = 0
+            
+            for char in sCharacters {
+                //find the char witch equal to the dic
+                if index < stringCharactes.count && stringCharactes[index] == char {
+                    index += 1
+                }
+            }
+            
+            if index == string.characters.count {
+                return string
+            }
+        }
+        
+        return ""
+    }
+}
+
+Solution().findLongestWord("abpcplea", ["ale","apple","monkey","plea", "aac"])
