55ef5c5492425391dc848a8708de49543d6f2bdf
Wilson <xiaochong2154@163.com>
2017-04-19 08:15:52 +0800

    update [Simplify Path]

---
+++ 71. Simplify Path.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: 71. Simplify Path
+/*:
+ Given an absolute path for a file (Unix-style), simplify it.
+ 
+ For example,
+ path = "/home/", => "/home"
+ path = "/a/./b/../../c/", => "/c"
+*/
+
+import Foundation
+
+class Solution {
+    func simplifyPath(_ path: String) -> String {
+        return ""
+    }
+}
