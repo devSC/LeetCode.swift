8205894471182730a73abd72398a175514739c3b
Wilson <xiaochong2154@163.com>
2017-05-21 21:23:53 +0800

    initial [Palindrome Partitioning]

---
+++ 131. Palindrome Partitioning.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*: 131. Palindrome Partitioning
+ Given a string s, partition s such that every substring of the partition is a palindrome.
+ 
+ Return all possible palindrome partitioning of s.
+ 
+ For example, given s = "aab",
+ Return
+ 
+ [
+ ["aa","b"],
+ ["a","a","b"]
+ ]
+*/
+import Foundation
+
+class Solution {
+    func partition(_ s: String) -> [[String]] {
+        
+    }
+}
