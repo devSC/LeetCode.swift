7e56f770ccade31d8860b61f197741702e34fcf8
Wilson <xiaochong2154@163.com>
2017-05-30 12:38:19 +0800

    update [Subsets]

--- 78. Subsets.playground/Contents.swift
+++ 78. Subsets.playground/Contents.swift
@@ -16,11 +16,46 @@
     [1,2],
     []
  ]
+ 
+ 整理后:
+ [
+    []
+ 
+    [1],
+ 
+    [2],
+    [1,2],
+ 
+    [3],
+    [1,3],
+    [2,3],
+    [1,2,3],
+ ]
+ 
+ 可以看到, 依次取出之前的值, 并且拼接上新值即可.
 */
 import Foundation
 
 class Solution {
     func subsets(_ nums: [Int]) -> [[Int]] {
-        return [[]]
+        
+        guard nums.count > 0 else {
+            return [[]]
+        }
+        
+        var results: [[Int]] = [[]]
+        
+        for i in 0 ..< nums.count {
+            let length = results.count
+            for j in 0 ..< length {
+                var list = results[j]
+                list.append(nums[i])
+                results.append(list)
+                print(i, length, j,  list)
+            }
+        }
+        return results
     }
 }
+
+Solution().subsets([1, 2, 3])
