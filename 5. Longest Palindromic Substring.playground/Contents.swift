871f7d9ed0a4b38696f01fdb68cb922de42a4f4e
Wilson <xiaochong2154@163.com>
2019-08-28 09:18:59 +0800

    UPDATE TO SWIFT 5

--- 5. Longest Palindromic Substring.playground/Contents.swift
+++ 5. Longest Palindromic Substring.playground/Contents.swift
@@ -21,9 +21,9 @@ Output: "bb"
 */
 
 private extension String {
-    subscript (range: Range<Int>) -> String {
-        return self[self.characters.index(self.startIndex, offsetBy: range.lowerBound)..<self.characters.index(self.startIndex, offsetBy: range.upperBound, limitedBy: self.endIndex)!]
-    }
+//    subscript (range: Range<Int>) -> String {
+//        return self[self.index(self.startIndex, offsetBy: range.lowerBound)..<self.index(self.startIndex, offsetBy: range.upperBound, limitedBy: self.endIndex)!]
+//    }
     /*
      Ref: http://oleb.net/blog/2014/07/swift-strings/
      "Because of the way Swift strings are stored, the String type does not support random access to its Characters via an integer index — there is no direct equivalent to NSStringʼs characterAtIndex: method. Conceptually, a String can be seen as a doubly linked list of characters rather than an array."
@@ -32,36 +32,36 @@ private extension String {
      we could hopefully achieve amortized O(1) time for random access.
      */
     func randomAccessCharactersArray() -> [Character] {
-        return Array(self.characters)
+        return Array(self)
     }
 }
 
 struct Solution {
     // t = O(N^2), s = O(1)
     static func longestPalindrome(_ s: String) -> String {
-        guard s.characters.count > 1 else {
+        guard s.count > 1 else {
             return s
         }
         var startIndex: Int = 0
         var maxLen: Int = 1
         var i = 0
         let charArr = s.randomAccessCharactersArray()
-        while i < s.characters.count {
-//            print("s.characters.count - i: \(s.characters.count - i), maxLen / 2: \(maxLen / 2)")
-            guard s.characters.count - i > maxLen / 2 else {
+        while i < s.count {
+//            print("s.count - i: \(s.count - i), maxLen / 2: \(maxLen / 2)")
+            guard s.count - i > maxLen / 2 else {
                 break
             }
             var j = i
             var k = i
             print("i: \(i), j: \(j), k: \(k)")
             //judge if string is like: aaaaa
-            while k < s.characters.count - 1 && charArr[k+1] == charArr[k] {
+            while k < s.count - 1 && charArr[k+1] == charArr[k] {
                 print("one: \(charArr[k+1]) == \(charArr[k])")
                 k += 1
             }
             i = k + 1
             print("i: \(i), j: \(j), k: \(k)")
-            while k < s.characters.count - 1 && j > 0 && charArr[k+1] == charArr[j-1] {
+            while k < s.count - 1 && j > 0 && charArr[k+1] == charArr[j-1] {
                 print("two: \(charArr[k+1]) == \(charArr[j-1]), j:\(j)")
                 k += 1
                 j -= 1
@@ -87,7 +87,7 @@ class Solutions {
         var maxPoint = 0
         
         charactersArr.append("$")
-        for character in s.characters {
+        for character in s {
             charactersArr.append("#")
             charactersArr.append(character)
         }
@@ -97,7 +97,7 @@ class Solutions {
         var rightMax = 0, middlePoint = 0
         var lenArray = Array(repeating: 1, count: charactersArr.count)
         lenArray.count
-        for i in 1 ..< 2 * s.characters.count + 2 {
+        for i in 1 ..< 2 * s.count + 2 {
             print("i: \(i)")
             if rightMax > i {
                 lenArray[i] = min(rightMax - i, lenArray[2 * middlePoint - i])
@@ -126,9 +126,9 @@ class Solutions {
 }
 
 
-//Solution.longestPalindrome("babad")
-//Solution.longestPalindrome("aabcd")
-//Solution.longestPalindrome("aabaada")
-//Solution.longestPalindrome("aaba")
+Solution.longestPalindrome("babad")
+Solution.longestPalindrome("aabcd")
+Solution.longestPalindrome("aabaada")
+Solution.longestPalindrome("aaba")
 Solution.longestPalindrome("cbbd")
 Solutions.longestPalindrome(s: "aabbcbbaa")
