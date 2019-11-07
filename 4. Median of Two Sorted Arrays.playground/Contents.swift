ef64fc0609f7cf3448deefbeb249feb12c64fcd0
袁仕崇 <xiaochong2154@163.com>
2019-11-07 18:59:05 +0800

    UPDATE [4. Median of Two Sorted Arrays]

--- 4. Median of Two Sorted Arrays.playground/Contents.swift
+++ 4. Median of Two Sorted Arrays.playground/Contents.swift
@@ -54,17 +54,81 @@ func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
 
 var nums1: [Int] = [1, 2]
 var nums2: [Int] = [3, 4]
-findMedianSortedArrays(nums1, nums2)
+//findMedianSortedArrays(nums1, nums2)
 
 nums1 = [1, 3]
 nums2 = [2]
 findMedianSortedArrays(nums1, nums2)
+//
+//nums1 = []
+//nums2 = [3]
+//findMedianSortedArrays(nums1, nums2)
+//
+//nums1 = [1, 1]
+//nums2 = [1, 3]
+//findMedianSortedArrays(nums1, nums2)
 
-nums1 = []
-nums2 = [3]
-findMedianSortedArrays(nums1, nums2)
+//查找无序数组中位数
+func findMedian(_ nums: inout [Int]) -> Int {
+    let low = 0
+    let high = nums.count - 1
+    let mid = high / 2
+    var div = partSort(&nums, start: low, end: high)
+    
+    while div != mid {
+        print("mid: \(mid) div: \(div) nums: \(nums)")
+        if mid < div {
+            //在左半区查找
+            div = partSort(&nums, start: low, end: div - 1)
+            
+        } else {
+            //y在右半区查找
+            div = partSort(&nums, start: div + 1, end: high)
+        }
+        print("mid: \(mid) div: \(div) nums: \(nums) ---")
+    }
+    return nums[mid]
+}
 
-nums1 = [1, 1]
-nums2 = [1, 3]
-findMedianSortedArrays(nums1, nums2)
+func partSort(_ nums: inout [Int], start: Int, end: Int) -> Int {
+    
+    let pivot = nums[start]
+    var left = start
+    var right = end
+    //5
+    while left < right {
+        //从右往左找，找到第一个比当前 pivot 值小的
+        while left < right && nums[right] > pivot {
+            right -= 1
+        }
+        if left < right {
+            nums[left] = nums[right]
+        }
+        //从左往右找，找到第一个比 pivot 值大的
+        while left < right && nums[left] < pivot {
+            left += 1
+        }
+        
+        if left < right {
+            nums[right] = nums[left];
+        } else {
+            //到达中间
+            nums[left] = pivot
+        }
+    }
+    
+    //返回中位 index
+    return left;
+}
+
+//6, 1, 2, 5, 4, 3
+var array = [12, 3, 10, 8, 6, 7, 11, 13, 9, 2]
+//3 6 7 8 9 10 11 12 13
+//        |
+//2 3 6 7 8 9 10 11 12 13
+
+//partSort(&array, start: 0, end: array.count - 1)
+
+findMedian(&array)
+
 
